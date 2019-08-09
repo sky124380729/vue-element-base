1f9a6272a86716e07a0a535efe1bee7a7065d7b2
pink <jerry.zheng@cloudkeeper.cn>
2019-08-09 16:54:28 +0800

    8/9修改

---
+++ src/pages/layout/Grid.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="grid">
+        <div class="grid__menu">
+            <Menu />
+        </div>
+        <div class="grid__header"></div>
+        <div class="grid__nav"></div>
+        <div class="grid__content"></div>
+    </div>
+</template>
+
+<script>
+import Menu from "./Menu";
+export default {
+    name: "grid",
+    components: {
+        Menu
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.grid {
+    display: grid;
+    width: 100vw;
+    height: 100vh;
+    grid-template-columns: 200px 1fr;
+    grid-template-rows: 50px 40px 1fr;
+    &__menu {
+        grid-row: 1 / -1;
+        background-color: #304156;
+    }
+    &__header {
+        background-color: #fff;
+        box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+    }
+    &__nav {
+        background-color: aquamarine;
+    }
+    &__content {
+        background-color: aqua;
+    }
+}
+</style>
---
+++ src/pages/layout/Menu.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="menu">
+        <img class="logo" src="~imgs/logo.png" alt="" />
+    </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+.menu {
+    .logo {
+        width: 100%;
+        height: 50px;
+        object-fit: none;
+    }
+}
+</style>
