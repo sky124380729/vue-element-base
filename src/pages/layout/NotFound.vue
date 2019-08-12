3d4688ead8872b174bc7a38937ea47a859161769
pink <jerry.zheng@cloudkeeper.cn>
2019-08-12 17:54:35 +0800

    404

---
+++ src/pages/layout/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+    <section style="color:#fff;">
+        <p>页面找不到了</p>
+        <a href="#">点我返回</a>
+    </section>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from 'pages/Home'
+import NotFound from 'pages/layout/NotFound'
 
 Vue.use(Router)
 
@@ -8,6 +9,15 @@ export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
+        {
+            path: '*',
+            redirect: '/404'
+        },
+        {
+            path: '/404',
+            name: 'notFound',
+            component: NotFound
+        },
         {
             path: '/',
             name: 'home',
