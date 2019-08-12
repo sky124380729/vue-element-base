ac8df418a89eee10a4bf18d3fb6b88b60fea704a
pink <jerry.zheng@cloudkeeper.cn>
2019-08-12 10:15:19 +0800

    目录结构整理

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import Element from "element-ui";
-import "./styles/index.scss"; // 全局 css
-import "element-ui/lib/theme-chalk/index.css"; // element css
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Element from 'element-ui'
+import './styles/index.scss' // 全局 css
+import 'element-ui/lib/theme-chalk/index.css' // element css
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 Vue.use(Element, {
-    size: "small"
-});
+    size: 'small'
+})
 
 new Vue({
     router,
     store,
     render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
