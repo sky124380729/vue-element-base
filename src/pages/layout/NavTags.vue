7818b9b848daacf8152d604f70c2582cb08b4474
pink <jerry.zheng@cloudkeeper.cn>
2019-10-08 16:57:51 +0800

    关于navTags

--- src/pages/layout/NavTags.vue
+++ src/pages/layout/NavTags.vue
@@ -1,25 +1,27 @@
 <template>
     <section>
         <div class="navTags">
-            <div class="navTags__btn navTags__btn--left">
+            <div class="navTags__btn navTags__btn--left" :class="{ 'navTags__btn--disabled': !moveAble }" @click="move(-1)">
                 <i class="el-icon-caret-left"></i>
             </div>
             <div class="navTags__box">
-                <el-tag
-                    @contextmenu.prevent.native="openMenu(item, $event)"
-                    @click="$router.push({ name: item.name })"
-                    :class="{ isActive: $route.path === item.path }"
-                    closable
-                    @close="closeTag(item)"
-                    size="medium"
-                    v-for="item in navTags"
-                    :key="item.name"
-                >
-                    <span class="dotted"></span>
-                    <span>{{ item.title }}</span>
-                </el-tag>
+                <div class="navTags__container" @mousewheel="mouseWheel" :style="{ transform: `translate(${this.posX}px, 0px)` }">
+                    <el-tag
+                        @contextmenu.prevent.native="openMenu(item, $event)"
+                        @click="$router.push({ name: item.name })"
+                        :class="{ isActive: $route.path === item.path }"
+                        closable
+                        @close="closeTag(item)"
+                        size="medium"
+                        v-for="item in navTags"
+                        :key="item.name"
+                    >
+                        <span class="dotted"></span>
+                        <span>{{ item.title }}</span>
+                    </el-tag>
+                </div>
             </div>
-            <div class="navTags__btn navTags__btn--right">
+            <div class="navTags__btn navTags__btn--right" :class="{ 'navTags__btn--disabled': !moveAble }" @click="move(1)">
                 <i class="el-icon-caret-right"></i>
             </div>
             <el-dropdown @command="handleTag">
@@ -41,6 +43,7 @@
 </template>
 
 <script>
+import { Debounce } from '@/utils/tools'
 import { mapGetters, mapMutations } from 'vuex'
 export default {
     name: 'navTags',
@@ -49,12 +52,22 @@ export default {
             visible: false,
             top: 0,
             left: 0,
+            index: 0,
+            tagWidth: [],
+            posX: 0,
             selectedTag: {}
         }
     },
     watch: {
-        $route(route) {
-            this.addTag(route)
+        $route: {
+            handler(route) {
+                this.addTag(route)
+                this.$nextTick(() => {
+                    const tags = document.querySelector('.navTags__container').childNodes
+                    this.tagWidth = [...tags].map(tag => Math.round(tag.getBoundingClientRect().width))
+                })
+            },
+            immediate: true
         },
         visible(value) {
             if (value) {
@@ -65,10 +78,24 @@ export default {
         }
     },
     computed: {
-        ...mapGetters(['navTags'])
+        ...mapGetters(['navTags']),
+        moveAble() {
+            return true
+        }
     },
     methods: {
         ...mapMutations(['ADD_NAVTAGS', 'DEL_NAVTAGS', 'DEL_OTHER_NAVTAGS', 'DEL_ALL_NAVTAGS']),
+        mouseWheel(e) {
+            Debounce(this.move(e.wheelDelta > 0 ? 1 : -1))
+        },
+        move(flag) {
+            if (!this.moveAble) return
+            if ((flag < 0 && this.index === this.tagWidth.length - 1) || (flag > 0 && this.index === 0)) return
+            const MARGIN_RIGHT = 5 // 右边距
+            const W = this.tagWidth[flag > 0 ? this.index - 1 : this.index]
+            this.posX += flag * (W + MARGIN_RIGHT)
+            this.index = this.index - flag
+        },
         handleTag(command) {
             if (command === 'closeOthers') {
                 this.selectedTag = this.navTags.find(v => v.path === this.$route.path)
@@ -84,10 +111,6 @@ export default {
                 path: route.path,
                 title: route.meta.title,
                 name: route.name
-            })
-            this.$nextTick(() => {
-                // let index = [...document.querySelectorAll('.tag-container .el-tag')].findIndex(item => item.classList.contains('isActive'))
-                // this.mySwiper.slideTo(index)
             })
         },
         // 删除标签
@@ -134,17 +157,12 @@ export default {
         openMenu(tag, e) {
             this.visible = true
             this.selectedTag = tag
-            // const offsetLeft = this.$el.getBoundingClientRect().left // container margin left
-            console.log(e.target)
             this.left = e.clientX
             this.top = e.clientY
         },
         closeMenu() {
             this.visible = false
         }
-    },
-    mounted() {
-        this.addTag(this.$route)
     }
 }
 </script>
@@ -159,8 +177,17 @@ export default {
     box-shadow: 0px 1px 10px 0px rgba(100, 100, 100, 0.2);
     flex-direction: start;
     font-size: 12px;
+    &__box {
+        overflow: hidden;
+    }
+    &__container {
+        transition: all 0.6s cubic-bezier(0.4, 0, 0.2, 1) 0s;
+    }
     &__btn {
         @include flex;
+        position: relative;
+        background-color: #fff;
+        z-index: 99;
         box-sizing: border-box;
         width: 28px;
         height: 28px;
@@ -172,9 +199,14 @@ export default {
         color: $-color--link;
         transition: color 0.3s;
         border-radius: 2px;
-        &:hover {
+        &:not(.navTags__btn--disabled):hover {
             background-color: $-color--link;
             color: #fff;
+            border-radius: 50%;
+        }
+        &--disabled {
+            border-color: #c2c5cc;
+            color: #c2c5cc;
         }
     }
     &__box {
@@ -222,7 +254,7 @@ export default {
     font-size: 12px;
     font-weight: 400;
     color: #333;
-    box-shadow: 2px 2px 3px 0 rgba(0, 0, 0, 0.3);
+    box-shadow: 3px 3px 13px 1px rgba(0, 0, 0, 0.2);
     li {
         margin: 0;
         padding: 7px 16px;
